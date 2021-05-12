c9f39a386784d3a420ddd4337165855d757d8efb
macOs <macos@macs-MBP.attlocal.net>
2021-05-11 22:44:59 -0700

    1st push

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log('Hello from js/scripts.js!');
+
+function imagefun() {
+            var Image_Id = document.getElementById('getImage');
+            if (Image_Id.src.match("../images/popup.png")) {
+                Image_Id.src = "../images/popup2.png";
+            }
+            else {
+                Image_Id.src = "../images/popup.png";
+            }
+        }
