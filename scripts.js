813b81b473c631a1865624bbdb997fc59a351e2d
macOs <macos@macs-MBP.attlocal.net>
2021-05-11 22:53:36 -0700

    2nd push

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ console.log('Hello from js/scripts.js!');
 
 function imagefun() {
             var Image_Id = document.getElementById('getImage');
-            if (Image_Id.src.match("../images/popup.png")) {
-                Image_Id.src = "../images/popup2.png";
+            if (Image_Id.src.match("images/popup.png")) {
+                Image_Id.src = "images/popup2.png";
             }
             else {
-                Image_Id.src = "../images/popup.png";
+                Image_Id.src = "images/popup.png";
             }
         }
